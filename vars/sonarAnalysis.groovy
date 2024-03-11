323ad35c36568bd2fc157c6d638f0ff2f911d2f8
baldomero osorio <124305954+Baldo15@users.noreply.github.com>
2024-03-11 17:44:40 -0500

    Update sonarAnalysis.groovy
    commit

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,3 +1,6 @@
-def call() {
+def call(Map params) {
     echo "Ejecución de las pruebas de calidad de código"
+    if (params.abortPipeline) {
+        error "Abortando el pipeline según el parámetro proporcionado."
+    }
 }
