a4e458996fb729f7f590f159fa58e1793c154447
baldomero osorio <124305954+Baldo15@users.noreply.github.com>
2024-03-11 17:35:14 -0500

    Update sonarAnalysis.groovy
    commit con comentarios

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,20 +1,35 @@
-
 def call(boolean abortPipeline = false, boolean qualityGateAbort = false) {
-    echo "Ejecución de las pruebas de calidad de código"
+    // Mensaje de inicio de la función
+    echo "Iniciando la función call"
 
     script {
+        // Sección de configuración de SonarQube
         withSonarQubeEnv('SonarQubeServer') {
+            // Configuración del ejecutable SonarScanner
             def scannerHome = tool 'SonarScanner';
             def scannerCmd = "${scannerHome}/bin/sonar-scanner"
+
+            // Ejecución de SonarScanner con configuraciones específicas
             sh "${scannerCmd} -Dsonar.projectKey=myProjectKey -Dsonar.sources=src"
+
+            // Espera para obtener el resultado del Quality Gate
             def qualityGateResult = waitForQualityGate()
+
+            // Mensaje con el resultado del Quality Gate
             echo "QualityGate Result: ${qualityGateResult}"
+
+            // Verificación y posible aborto del pipeline según la configuración
             if (qualityGateAbort && qualityGateResult != 'OK') {
                 error "QualityGate de SonarQube falló. Abortando el pipeline."
             }
         }
+
+        // Verificación y posible aborto del pipeline según la configuración
         if (abortPipeline) {
             error "Abortando el pipeline según el parámetro proporcionado."
         }
     }
+
+    // Mensaje de finalización de la función
+    echo "Finalizando la función call"
 }
