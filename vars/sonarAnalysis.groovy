b8822742f39a27af482698498e4258c7b86e50ee
baldomero osorio <124305954+Baldo15@users.noreply.github.com>
2024-03-13 13:50:26 -0500

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,10 +1,8 @@
 def call(boolean qualityGateAbort = false, boolean abortPipeline = false) {
     echo "Ejecución de las pruebas de calidad de código"
-    return
-    // Puedes sustituir este bloque con la lógica de SonarQube si decides usarlo
-    // sh 'sonar-scanner' (o cualquier comando de SonarQube)
+    echo "qualityGateAbort: ${qualityGateAbort}"
+    echo "abortPipeline: ${abortPipeline}"
     
-    // Mantén el sonarenv y el timeout
     withSonarQubeEnv('SonarQube Server') {
         timeout(time: 5, unit: 'MINUTES') {
             // Puedes sustituir este bloque con el comando real de SonarQube si decides usarlo
@@ -12,8 +10,7 @@ def call(boolean qualityGateAbort = false, boolean abortPipeline = false) {
             echo "Simulación de escaneo de SonarQube"
         }
     }
-
-    // Lógica para evaluar el QualityGate y abortar el pipeline según los parámetros
+    
     if (qualityGateAbort) {
         // Puedes agregar aquí la lógica para evaluar el QualityGate de SonarQube
         // Ejemplo: sh 'sonar-scanner -Dsonar.qualitygate.wait=true'
@@ -22,10 +19,10 @@ def call(boolean qualityGateAbort = false, boolean abortPipeline = false) {
         error "QualityGate no aprobado. Abortando el pipeline."
         return
     }
-
-    // Abortar el pipeline según el parámetro recibido
+    
     if (abortPipeline) {
         error "Abortando el pipeline según el parámetro proporcionado."
         return
     }
 }
+
